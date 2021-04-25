970b3bcbe02211566a35ee2ce12045e80ffdb614
Tidbits JS <tidbitsjs@gmail.com>
2021-04-25 21:11:59 +0530

    proper routing

--- server/route/articles.js
+++ server/route/articles.js
@@ -2,45 +2,45 @@ const express = require("express");
 const Article = require("../model/article");
 const router = express.Router();
 
-router.get("/new", (req, res) => {
-  res.render("/articles/new", { article: new Article() });
+// router.get("/new", (req, res) => {
+//   res.render("/articles/new", { article: new Article() });
+// });
+
+router.route("/edit/:id").get(async (req, res) => {
+  try {
+    const article = await Article.findById(req.params.id);
+    res.json(article);
+  } catch (e) {
+    res.status(400).json("Error: " + err);
+  }
+  //   res.render("/articles/edit", { article: article });
 });
 
-router.get("/edit/:id", async (req, res) => {
-  const article = await Article.findById(req.params.id);
-  res.render("/articles/edit", { article: article });
+router.route("/:slug").get(async (req, res) => {
+  try {
+    const article = await Article.findOne({ slug: req.params.slug });
+    res.json(article);
+  } catch (e) {
+    res.status(400).json("Error: " + err);
+  }
 });
 
-router.get("/:slug", async (req, res) => {
-  const article = await Article.findOne({ slug: req.params.slug });
-  if (article === null) res.redirect("/");
-  res.render("/articles/show", { article: article });
-});
+router.route("/").post(async (req, res, next) => {
+  req.article = new Article();
+  next();
+}, saveArticleAndRedirect("new"));
 
-router.post(
-  "/",
-  async (req, res, next) => {
-    req.article = new Article();
-    next();
-  },
-  saveArticleAndRedirect("new")
-);
+router.route("/:id").put(async (req, res, next) => {
+  req.article = await article.findById(req.params.id);
+  next();
+}, saveArticleAndRedirect("edit"));
 
-router.put(
-  "/:id",
-  async (req, res, next) => {
-    req.article = await article.findById(req.params.id);
-    next();
-  },
-  saveArticleAndRedirect("edit")
-);
-
-router.delete("/:id", async (req, res) => {
+router.route("/:id").delete(async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect("/");
 });
 
-const saveArticleAndRedirect = (path) => {
+function saveArticleAndRedirect(path) {
   return async (req, res) => {
     let article = req.article;
     article.title = req.body.title;
@@ -48,11 +48,11 @@ const saveArticleAndRedirect = (path) => {
     article.markdown = req.body.markdown;
     try {
       article = await article.save();
-      res.redirect(`/articles/${article.slug}`);
+      res.json("Blog added");
     } catch (e) {
-      res.render(`articles/${path}`, { article: article });
+      res.redirect(`articles/${path}`);
     }
   };
-};
+}
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 
+const Article = require("./model/article");
+const ArticleRouter = require("./route/articles");
 require("dotenv").config();
 
 const app = express();
@@ -10,7 +12,11 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGO_URI;
 mongoose
-  .connect(uri, { useUnifiedTopology: true, useCreateIndex: true })
+  .connect(uri, {
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+  })
   .then(() => console.log("Successfully connected to mongodb"))
   .catch((err) => {
     throw new error("Oops an error connecting to MongoDB", err);
@@ -18,6 +24,17 @@ mongoose
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride("_method"));
+
+app.route("/").get(async (req, res) => {
+  try {
+    const articles = await Article.find().sort({ createdAt: "desc" });
+    res.json(articles);
+  } catch (e) {
+    res.status(400).json("Error: " + err);
+  }
+});
+
+app.use("/articles", ArticleRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port:  ${port}`);
